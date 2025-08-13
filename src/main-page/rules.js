08a0b519fe34b02e5df3b4373527d74b951871f5
EgorGer <QwertiQQQ@gmal.com>
2025-08-13 12:51:54 +0300

    feat: rules block

---
+++ src/main-page/rules.js
@@ -0,0 +1,29 @@
+function createRulesBlock() {
+  const rules = [
+    "1. Не рассказывать о Бойцовском клубе.",
+    "2. Не рассказывать о Бойцовском клубе.",
+    '3. Если кто-то сказал "стоп", потерял сознание или не может продолжать — бой окончен.',
+    "4. В бою участвуют только двое.",
+    "5. Один бой за раз.",
+    "6. Бойцы сражаются без обуви и без рубашек.",
+    "7. Бой длится столько, сколько потребуется.",
+    "8. Если это твой первый вечер в Бойцовском клубе — ты должен драться.",
+  ];
+
+  const rulesContainer = document.createElement("div");
+  rulesContainer.className = "rules-container";
+
+  let delay = 0;
+  rules.forEach((rule) => {
+    const p = document.createElement("p");
+    p.textContent = rule;
+    p.className = "rule-text";
+    p.style.animationDelay = `${delay}s`;
+    rulesContainer.appendChild(p);
+    delay += 2;
+  });
+
+  return rulesContainer;
+}
+
+export { createRulesBlock };
